5183dc3908d390668164441441ff68cd58b82b6d
KillerGoyal <96700176+KillerGoyal@users.noreply.github.com>
2021-12-31 20:18:30 +0530

    Add files via upload

---
+++ KillerSk/misc.py
@@ -0,0 +1,23 @@
+HELP_TEXT = """__**I Can Play Music In The Voice Chat**__
+
+**/start** __To Start The bot.__
+**/help** __To Show This Message.__
+**/ping** __To Show Latency Bot.__
+**/skip** __To Skip The Current Playing Music.__
+**/play** __youtube/saavn/deezer song_name__
+**/stop** __To Stop Playing Music.__
+**/join** __To Join A Voice Chat.__
+**/leave** __To Leave A Voice Chat.__
+**/mute** __To Mute A Bot.__
+**/unmute** __To Unmute A Bot.__
+**/volume** __<1-200>__
+**/kill** __To Kill A Sevice Bot.__
+**/repo** __To Source Code This Not.__
+**/donation** __To Give Me A Coffe.__"""
+
+START_TEXT = "__**Hi I'm Friend's Book Music Bot. I will sing for you on Voice Call Telegram, Hit /help for look my features.**__"
+
+REPO_TEXT = """HA HA kid ! \nAise hi thodi na mil jaaaega , khudse dhund jaake noob!"""
+
+DONATION_TEXT = """Message here for donaing @souravkkk
+"""
